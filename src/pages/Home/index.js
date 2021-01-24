fe8c6ba7c04f60d19bcf7d1658a5ca3ae918bdf4
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2021-01-23 23:03:15 -0300

    feat: finish task

---
+++ src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from 'react';
+
+import './index.css';
+
+const Home = () => {
+  const [repos, setRepos] = useState([]);
+
+  const handleSubmit = useCallback(() => {
+    // CODE
+  }, []);
+
+  console.log(repos);
+
+  return (
+    <div className="container">
+      <main className="search-group">
+        <span>Git <br /> Finder</span>
+
+        <form className="button-group" onSubmit={handleSubmit}>
+          <input type="text" />
+          <button>Procurar</button>
+        </form>
+      </main>
+
+      <aside className="list-repos">
+        <div className="repo-wrapper">
+          {repos.map((repo) => (
+            <a href="/">
+              {repo.name}
+            </a>
+          ))}
+        </div>
+      </aside>
+    </div>
+  );
+}
+
+export default Home;
